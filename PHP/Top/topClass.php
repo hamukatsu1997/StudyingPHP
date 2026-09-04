a789e8679c9fc1ad1a4b00b8e6070a543f72da08
TAKEDA-PRN\ishida_haruki <ishida_haruki@takeda-prn.co.jp>
2026-09-04 17:47:07 +0900

    [ADD]カラオケリストの追加

---
+++ PHP/Top/topClass.php
@@ -0,0 +1,44 @@
+<?php
+class Top
+{
+public function displayKaraoke()
+{
+    if (!isset($_SESSION['userId'])) {
+        header('Location: ' . BASE_URI_USER . '/');
+        exit;
+    }
+
+    $this->userId = $_SESSION['userId'];
+    $this->userName = $_SESSION['userName'];
+
+    $file = __DIR__ . '/../../HTML/Karaoke/karaoke.html';
+
+        if (!file_exists($file)) {
+            http_response_code(500);
+            exit('カラオケ画面が見つかりません: ' . $file);
+        }
+
+        include $file;
+        exit;
+}
+
+
+    public function jsonUpdateKeisaiKibouFlg()
+    {
+        if(!isset($_SESSION['userId'])){
+            header('Location: ' .BASE_URI_USER . '/');
+            exit;
+        }
+
+                $userId = $_SESSION['userId'];
+        $userName = $_SESSION['userName'];
+
+        // カラオケリストを取得する場合
+        // $karaokeList = ...
+
+
+    }
+}
+
+?>
+
